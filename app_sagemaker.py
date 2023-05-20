f772ff3541ef245caf601372d36533032ccfd113
Heiko Hotz <heiko.hotz@posteo.net>
2023-05-20 19:50:32 +0100

    initial commit

--- app_sagemaker.py
+++ app_sagemaker.py
@@ -11,11 +11,12 @@ from random import randint
 st.set_page_config(page_title="Document Analysis", page_icon=":robot:")
 st.header("Chat with your document 📄")
 
-
+# the content handler defines on how the input and output to the SM endpoint will be parsed
 class ContentHandler(ContentHandlerBase):
     content_type = "application/json"
     accepts = "application/json"
 
+    # for this chatbot demo we hardcode the inference parameters
     def transform_input(self, prompt: str, model_kwargs: Dict) -> bytes:
         input_str = json.dumps({"prompt": prompt, "maxTokens": 100, "temperature": 0, "stopSequences": ["Human"]})
         return input_str.encode('utf-8')
@@ -36,7 +37,7 @@ def load_chain():
     llm = SagemakerEndpoint(
         endpoint_name=endpoint_name,
         region_name="us-east-1",
-        model_kwargs={"temperature": 1e-10},
+        # model_kwargs={"temperature": 1e-10},
         content_handler=content_handler
     )
 
@@ -44,9 +45,10 @@ def load_chain():
     chain = ConversationChain(llm=llm, memory=memory)
     return chain
 
-
+# this is the object we will work with in the ap - it contains the LLM info as well as the memory
 chatchain = load_chain()
 
+# initialise session variables
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
 if 'past' not in st.session_state:
@@ -55,6 +57,7 @@ if 'past' not in st.session_state:
 if 'widget_key' not in st.session_state:
     st.session_state['widget_key'] = str(randint(1000, 100000000))
 
+# Sidebar - the clear button is will flush the memory of the conversation
 st.sidebar.title("Sidebar")
 clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
@@ -64,20 +67,26 @@ if clear_button:
     st.session_state['widget_key'] = str(randint(1000, 100000000))
     chatchain.memory.clear()
 
+# upload file button
 uploaded_file = st.sidebar.file_uploader("Upload a txt file", type=["txt"], key=st.session_state['widget_key'])
 
+# this is the container that displays the past conversation
 response_container = st.container()
+# this is the container with the input text box
 container = st.container()
 
 with container:
+    # define the input text box
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_area("You:", key='input', height=100)
         submit_button = st.form_submit_button(label='Send')
 
+    # when the submit button is pressed we send the user query to the chatchain object and save the chat history
     if submit_button and user_input:
         output = chatchain(user_input)["response"]
         st.session_state['past'].append(user_input)
         st.session_state['generated'].append(output)
+    # when a file is uploaded we also send the content to the chatchain object and ask for confirmation
     elif uploaded_file is not None:
         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
         content = stringio.read()
@@ -86,6 +95,7 @@ with container:
         st.session_state['past'].append("I have uploaded a file. Please confirm that you have read that file.")
         st.session_state['generated'].append(output)
 
+# this loop is responsible for displaying the chat history
 if st.session_state['generated']:
     with response_container:
         for i in range(len(st.session_state['generated'])):
